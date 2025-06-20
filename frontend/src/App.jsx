3ba448f20504295411478157e2430cada7f8f734
mtstmd <mtstmd@gmail.com>
2025-06-19 22:24:30 -0300

    Fluxo de Telas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
-import Layout from "./components/Layout"
-import BackButton from "./components/BackButton"
 import Home from "./containers/Home"
 import Agendamento from "./containers/Agendamento"
 import MeusAgendamentos from "./containers/MeusAgendamentos"
 import RelatorioAdmin from "./containers/RelatorioAdmin"
-
 
 
 function App() {
@@ -13,17 +10,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* Rotas filhas que serão renderizadas dentro do Layout */}
-          {/* Usando caminhos em minúsculo como convenção */}
           <Route index element={<Home />} />
           <Route path="agendamento" element={<Agendamento />} />
           <Route path="meus-agendamentos" element={<MeusAgendamentos />} />
           <Route path="relatorio-admin" element={<RelatorioAdmin />} />
-        </Route>
       </Routes>
     </BrowserRouter>
   )
 }
 
 export default App
+
